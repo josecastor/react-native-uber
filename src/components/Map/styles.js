d27fd9a98c859a77dc576971d5f78bd3f2c65541
user154880 <user154880@74-80-250-215.ic2net.net>
2019-06-11 17:51:24 -0700

    Defined styled for text and box location

---
+++ src/components/Map/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native'
+
+export const LocationBox = styled.View`
+    background: #FFF;
+    shadow-color: #000;
+    shadow-offset: 0 0;
+    shadow-opacity: 0.1;
+    elavation: 1;
+    border: 1px solid #ddd;
+    border-radius: 3px;
+    flex-direction: row;
+`;
+export const LocationText = styled.Text`
+    margin: 8px 10px;
+    font-size: 14px;
+    color: #333;
+`;
+export const LocationBox = styled.View``;
